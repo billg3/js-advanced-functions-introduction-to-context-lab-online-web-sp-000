0994f6557551f8ca65ec7d19e3151e4d2ce812b5
Bill G <gbilly3@gmail.com>
2019-11-18 18:41:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,12 @@
 // the payroll system
 
 let createEmployeeRecord = function (row ){
-
-
- }
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
